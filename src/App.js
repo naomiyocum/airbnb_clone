a775e154464b6e666e1aeb3394f5f713319cc4f5
Naomi Yocum <naomiyocum24@gmail.com>
2023-05-05 15:21:41 -0600

    Complete Hero component and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Card from './components/Card'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
+      <Card />
     </div>
   );
 }
