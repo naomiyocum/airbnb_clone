bcb02865db75b2ea94c56b546010d1b42fc291ea
Naomi Yocum <naomiyocum24@gmail.com>
2023-05-11 08:24:40 -0700

    map data into card components

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ export default function Card(props) {
         <img src={require("../images/star.png")} alt="star"/>
         <span>{props.rating}</span>
         <span className="grey">({props.reviewCount}).</span>
-        <span className="grey">{props.country}</span>
+        <span className="grey">{props.location}</span>
       </div>
       <p>{props.title}</p>
       <p><span className="bold">From ${props.price} </span>/ person</p>
